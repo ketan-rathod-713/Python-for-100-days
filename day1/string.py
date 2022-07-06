872bf7b91bcbef3ac7206d45c5b6897bd5023d83
ketan <ketanrtd1@gmail.com>
2022-07-07 00:12:18 +0530

    First commit

---
+++ day1/string.py
@@ -0,0 +1,9 @@
+print("Print in 3 lines \nThis is second \tline")
+# I can concatinate the two string 
+print("hello"+"angela")
+
+# THis is another comment
+
+a = input("what is your name\n")  # For taking input 
+print("The typed value is : \n " + a)
+
---
+++ first.py
@@ -0,0 +1,8 @@
+import array
+from multiprocessing.dummy import Array
+
+
+print("just")
+print("This is just a print statement")
+a = "This is the string"
+print(a.split())
