7c48365c1b0f32a497a68967b234bb085139b43e
ketan <ketanrtd1@gmail.com>
2022-07-07 00:40:01 +0530

    first day strings and variables

--- day1/string.py
+++ day1/string.py
@@ -7,3 +7,17 @@ print("hello"+"angela")
 a = input("what is your name\n")  # For taking input 
 print("The typed value is : \n " + a)
 
+
+# I think this is working git 
+
+print("hello "+input("what is your name : ")+"!")
+
+print(len("just"))
+
+print(len(input("enter anything ")))
+
+# len function gives the length of the string
+
+a = "Just a string"
+
+print(len(a))
---
+++ day1/variables.py
@@ -0,0 +1,6 @@
+# variable to store items to refer item later
+
+name = input("Enter name ")
+name = "Ketan Rathod"
+print(name)
+# Just like the other programming languages ha ha
