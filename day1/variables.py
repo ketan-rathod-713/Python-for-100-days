f01067df0295718c5ad9867b9128b955fbfb739e
ketan <ketanrtd1@gmail.com>
2022-07-07 00:42:26 +0530

    first day strings and variables

--- day1/variables.py
+++ day1/variables.py
@@ -4,3 +4,11 @@ name = input("Enter name ")
 name = "Ketan Rathod"
 print(name)
 # Just like the other programming languages ha ha
+
+# variable naming
+# Rules
+# Make your code readable
+# user_name but cant have space in between 
+# cant use numbers in beginning that is syntax word
+# some privilage words like print and all cant be used 
+# if name error then mis name of variable used 
---
+++ day2/datatypes.py
@@ -0,0 +1,2 @@
+# already learned about string it is string of characters and we can pull out each character
+print("Hello"[0])
