369e8598ba6cd4d85392500e8f8d885675105950
ketan <ketanrtd1@gmail.com>
2022-07-07 11:11:48 +0530

    Day2 f strings , type , round , ** , // , typecasting

--- day2/datatypes.py
+++ day2/datatypes.py
@@ -1,2 +1,39 @@
 # already learned about string it is string of characters and we can pull out each character
 print("Hello"[0])
+
+# if number not written in double quotes then it will thing it as int not string
+
+# Integer Float Bool and so on 
+
+# len(1234) it will produce an error , this function doesn't like to work with this type of datatype
+
+a = "sstring"
+
+print(type(a))
+print(type(123))
+
+# also we can do typeCasting
+
+newStringofInt = str(123)  #converting numbers to string
+print(type(newStringofInt))
+
+a =float(123) #converted to float 
+
+float("100.5") # we are converting string to float in this , and so on for others 
+
+# Other mathematical operations
+# just like other programming lang.add()
+# whenever we dividing things we get float not integer 
+#  ** for power like 2**3 which is 8 
+
+print(2**3)
+
+# when more then one operation on same line then rule PEMDAS
+
+# () 
+# **
+# * / , calculation goes from left to right if both are at same level 
+# + -
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3)/3 - 3) # now the priority of the bracket is more 
---
+++ day2/numberManipulationAndFString.py
@@ -0,0 +1,12 @@
+print(round(8/3,2))  # to round it to two decimal numbers 
+print(8 // 3) # it is flow division , it will not return float instead it will return int that's all 
+
+result = 4/2
+result /= 2
+print(result)
+
+# += -= *= /= and so on 
+
+# F string
+print("your score is "+ str(result))
+print(f"your score is {result}")
