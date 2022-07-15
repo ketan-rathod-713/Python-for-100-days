d1ffaab35fa7b08df09456e054003e1db8a34060
ketan <ketanrtd1@gmail.com>
2022-07-15 16:31:18 +0530

    blackjack

---
+++ Resources.md
@@ -0,0 +1,4 @@
+
+ Important resources
+1. https://developers.google.com/edu/python/introduction
+2. https://docs.python.org/3/library/functions.html#sum
--- first.py
+++
@@ -1,8 +0,0 @@
-import array
-from multiprocessing.dummy import Array
-
-
-print("just")
-print("This is just a print statement")
-a = "This is the string"
-print(a.split())
